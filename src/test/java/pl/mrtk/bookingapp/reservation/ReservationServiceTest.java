052bc5df3dba3f013015627e91740e2f3f067ac5
mrtk <durna@wp.pl>
2021-11-09 20:52:17 +0100

    add unit test

---
+++ src/test/java/pl/mrtk/bookingapp/reservation/ReservationServiceTest.java
@@ -0,0 +1,22 @@
+package pl.mrtk.bookingapp.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class ReservationServiceTest {
+
+    private final ReservationRepository repository = Mockito.mock(ReservationRepository.class);
+    private final ReservationService service = new ReservationService(repository);
+
+    @Test
+    public void testCreateReservation() {
+        // given
+        ReservationData reservationData = Mockito.mock(ReservationData.class);
+
+        // when
+        service.createReservation(reservationData);
+
+        // then
+        Mockito.verify(repository, Mockito.times(1)).add(reservationData);
+    }
+}
